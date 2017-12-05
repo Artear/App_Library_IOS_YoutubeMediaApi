9e15107171185c710c57f7b6154cf0bcf7e5ab3e
Mauro Gonzalez <mauro.d.gonzalez90@gmail.com>
2017-12-05 14:18:21 -0300

    Add: Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-YoutubeApiMedia_Example/Pods-YoutubeApiMedia_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YoutubeApiMedia_Example : NSObject
+@end
+@implementation PodsDummy_Pods_YoutubeApiMedia_Example
+@end
---
+++ Example/Pods/Target Support Files/YoutubeApiMedia/YoutubeApiMedia-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YoutubeApiMedia : NSObject
+@end
+@implementation PodsDummy_YoutubeApiMedia
+@end
