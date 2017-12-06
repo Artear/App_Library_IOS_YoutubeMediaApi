c6e48e4a65e127be6353f5d408a6b5f120c6d8cf
Jose Luis Sagredo <jsagredo.ing@gmail.com>
2017-12-06 20:39:47 -0300

    Remove: Pod Example Add: ignore pod Example

--- Example/Pods/Target Support Files/Pods-YoutubeApiMedia_Tests/Pods-YoutubeApiMedia_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YoutubeApiMedia_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_YoutubeApiMedia_Tests
-@end
--- Example/Pods/Target Support Files/YoutubeApiMedia/YoutubeApiMedia-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_YoutubeApiMedia : NSObject
-@end
-@implementation PodsDummy_YoutubeApiMedia
-@end
